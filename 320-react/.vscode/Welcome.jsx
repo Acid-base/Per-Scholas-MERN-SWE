d69563f1e2edb3bd54ed5af734bbc63d6cb57bbc
Daniel Yates <145247690+Acid-base@users.noreply.github.com>
2024-05-06 11:15:41 -0600

    commit

---
+++ 320-react/.vscode/Welcome.jsx
@@ -0,0 +1,14 @@
+
+// welcome.jsx
+
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to my React App!</h1>
+    </div>
+  );
+};
+
+export default Welcome;
